933e521636cb7a9de8d4db0a5c7b529aec656339
russell.johnson <encrypteddata0@gmail.com>
2020-06-22 12:40:48 -0700

    Added player api

---
+++ src/main/java/net/superdark/minecraft/plugins/SuperDarkCore/api/PlayerAPI.java
@@ -0,0 +1,14 @@
+package net.superdark.minecraft.plugins.SuperDarkCore.api;
+
+import net.superdark.minecraft.plugins.SuperDarkCore.SuperDarkCorePlugin;
+
+public class PlayerAPI
+{
+    public PlayerAPI(SuperDarkCorePlugin plugin)
+    {
+        superDarkCorePlugin_ = plugin;
+    }
+
+    private SuperDarkCorePlugin superDarkCorePlugin_;
+
+}
