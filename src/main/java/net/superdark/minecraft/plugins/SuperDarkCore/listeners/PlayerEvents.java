1581da96f57fd7419b5e986df063a3c4e6a44372
Casey Johnson <onnesh1@gmail.com>
2020-09-18 10:55:19 -0700

    Added listener for Death messages Updated language for post message Added comments Added config option to disable the webhook Added a config string for the server tag.

--- src/main/java/net/superdark/minecraft/plugins/SuperDarkCore/listeners/PlayerEvents.java
+++ src/main/java/net/superdark/minecraft/plugins/SuperDarkCore/listeners/PlayerEvents.java
@@ -6,9 +6,11 @@ import net.superdark.minecraft.plugins.SuperDarkCore.api.WebhookAPI;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.jetbrains.annotations.Nullable;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -28,6 +30,7 @@ public class PlayerEvents implements Listener {
         this.plugin_ = instance_;
         this.playerAPI_ = playerAPI_;
         this.webhookAPI_ = this.plugin_.getWebhookAPI_();
+        this.serverTag = this.plugin_.getConfig().getString("serverTag");
 
         //player can be connected before a plugin register, so check for players and add them.
         for (Player p : instance_.getServer().getOnlinePlayers())
@@ -45,7 +48,7 @@ public class PlayerEvents implements Listener {
 
         //For default webhook
 
-        webhookAPI_.postDiscordMessage(getDiscordWebookURL(), "**[Server] " + e.getPlayer().getDisplayName() + " has joined the server.**");
+        webhookAPI_.postDiscordMessage(getDiscordWebookURL(), "**[Server] " + e.getPlayer().getDisplayName() + " has joined the server.**", serverTag);
     }
 
     @EventHandler
@@ -54,19 +57,31 @@ public class PlayerEvents implements Listener {
         playerAPI_.unregisterPlayer(e.getPlayer().getName());
 
         //For Default Webhook
-        webhookAPI_.postDiscordMessage(getDiscordWebookURL(), "**[Server] " + e.getPlayer().getDisplayName() + " has left the server.**");
+        webhookAPI_.postDiscordMessage(getDiscordWebookURL(), "**[Server] " + e.getPlayer().getDisplayName() + " has left the server.**", serverTag);
     }
 
     @EventHandler
     private void onAsyncPlayerChatEvent(AsyncPlayerChatEvent e) throws MalformedURLException
     {
-        webhookAPI_.postDiscordMessage(getDiscordWebookURL(), "**<" + e.getPlayer().getDisplayName() + ">**: " + e.getMessage());
+        webhookAPI_.postDiscordMessage(getDiscordWebookURL(), "**<" + e.getPlayer().getDisplayName() + ">**: " + e.getMessage(), serverTag);
     }
 
-    private URL getDiscordWebookURL() throws MalformedURLException
-    {
+    @EventHandler
+    private void onPlayerDeathEvent(PlayerDeathEvent e) throws MalformedURLException {
+        webhookAPI_.postDiscordMessage(getDiscordWebookURL(), "**[Server] Player " + e.getEntity().getPlayer().getName() + " has died.**", serverTag);
+    }
 
-        return new URL(Objects.requireNonNull(this.plugin_.getSuperDarkCoreConfig().getString("DiscordWebhook")));
+    private @Nullable URL getDiscordWebookURL() {
+        URL url;
+        try {
+            url = new URL(Objects.requireNonNull(this.plugin_.getSuperDarkCoreConfig().getString("DiscordWebhook")));
+        } catch (MalformedURLException e) {
+            this.plugin_.getLogger().severe("You have provided a malformed Webhook URL. Please change it in the config.");
+            return null;
+        }
+
+
+        return url;
     }
 
     private SuperDarkCorePlugin plugin_;
@@ -74,4 +89,6 @@ public class PlayerEvents implements Listener {
     private PlayerAPI playerAPI_;
 
     private WebhookAPI webhookAPI_;
+
+    private String serverTag;
 }
